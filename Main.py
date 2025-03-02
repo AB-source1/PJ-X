77faa4f2e3c395de15ee490ba3ec571ea2a43a4b
AB-source1 <a.bykov242@gmail.com>
2025-03-02 10:25:36 +0000

    add extract dates function

--- Main.py
+++ Main.py
@@ -3,6 +3,26 @@ import datetime
 import csv
 import argparse
 
+def extract_dates(file_path):
+    start_date = None
+    end_date = None
+    with open(file_path,"r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            key = row[0].strip().lower()
+            value = row[1].strip()
+            
+            if key == "start_date":
+                start_date = value
+            
+            elif key == "end_date":
+                end_date = value
+            
+    if not start_date or not end_date:
+    raise ValueError("Start Date or End Date not found in the CSV.")
+    
+    return start_date,end_date
+
 print(sys.argv)
 start_date = sys.argv[1]
 end_date = sys.argv[2]
